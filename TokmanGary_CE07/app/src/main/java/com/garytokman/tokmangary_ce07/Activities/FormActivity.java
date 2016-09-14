1c3c4c918edd7b06863413f8429df580e23651a2
gtokman <gtokman@fullsail.edu>
2016-09-14 12:37:38 -0400

    lab 7 initial commit

---
+++ TokmanGary_CE07/app/src/main/java/com/garytokman/tokmangary_ce07/Activities/FormActivity.java
@@ -0,0 +1,15 @@
+package com.garytokman.tokmangary_ce07.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.garytokman.tokmangary_ce07.R;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
---
+++ TokmanGary_CE07/app/src/main/java/com/garytokman/tokmangary_ce07/Activities/ListActivity.java
@@ -0,0 +1,15 @@
+package com.garytokman.tokmangary_ce07.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.garytokman.tokmangary_ce07.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
